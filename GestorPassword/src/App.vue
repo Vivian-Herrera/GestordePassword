8df89a7556538c271651d08009a177831f125151
Vivian-Herrera <vivian.herrera@ucb.edu.bo>
2025-09-24 21:00:27 -0400

    se agreguo el nombre de la alumna

--- GestorPassword/src/App.vue
+++ GestorPassword/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1 style="color:#542379;">Gestor de Contraseñas</h1>
+    <h5 style="color:#446844;">Alumno: Vivian Amparo Herrera Aduviri</h5>
     <PasswordManager />
   </div>
 </template>
